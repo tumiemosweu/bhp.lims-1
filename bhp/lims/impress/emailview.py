dc6fd65874699921ea029bdb86844e8a3b195b0b
MoffatMore <12894381+MoffatMore@users.noreply.github.com>
2019-03-13 20:31:14 +0200

    Adding pid in subject email (#206)
    * adding pid in subject email
    
    * fixing email results
    
    * deleting some attributes in emailview.py
    
    * Cleanup imports

--- bhp/lims/impress/emailview.py
+++ bhp/lims/impress/emailview.py
@@ -2,14 +2,26 @@
 #
 # Copyright 2018-2019 Botswana Harvard Partnership (BHP)
 
-from bika.lims.browser.publish.emailview import EmailView as BaseEmailView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from bika.lims.browser.publish.emailview import EmailView as BaseEmailView
 
 
 class EmailView(BaseEmailView):
     """Email Attachments View
     """
     email_template = ViewPageTemplateFile("templates/email_template.pt")
+
+    def __init__(self, context, request):
+        super(EmailView, self).__init__(context, request)
+        participant_ids = self.get_participant_ids()
+        self.email_subject = "Analysis Results for {}".format(", ".join(participant_ids))
+
+    def get_participant_ids(self):
+        """Participant IDs of the reports to be included in the email
+        """
+        ars = map(lambda rep: rep.getAnalysisRequest(), self.get_reports())
+        pids = map(lambda ar: ar.ParticipantID, ars)
+        return list(set(pids))
 
     def get_recipients(self, ar):
         """Returns the recipients the results report must be send to:
